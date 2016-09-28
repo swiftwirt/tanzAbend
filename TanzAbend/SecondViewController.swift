8edd32310458227cd7d968eaad77d58240ae9585
swiftwirt <swiftwirt@gmail.com>
2016-09-28 13:14:21 +0300

    Initial Commit

---
+++ TanzAbend/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TanzAbend
+//
+//  Created by Ivashin Dmitry on 9/28/16.
+//  Copyright © 2016 Ivashin Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
